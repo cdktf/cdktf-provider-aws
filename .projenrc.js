79b15bfc3c7c2b64f2d91537bf6fd7a944fa041e
Sebastian Korfmann <sebastian@korfmann.net>
2020-08-04 22:53:56 +0200

    Setup cdktf-project

--- .projenrc.js
+++ .projenrc.js
@@ -1,11 +1,13 @@
 const { CdktfProviderProject } = require('cdktf-provider');
+const { PROJEN_VERSION } = require('projen')
 
 const project = new CdktfProviderProject({
   name: "@skorfmann/cdktf-provider-aws",
   authorName: "Sebastian Korfmann",
   authorEmail: "sebastian@korfmann.net",
   repository: "https://github.com/skorfmann/cdktf-provider-aws.git",
-  terraformProvider: "aws@~> 2.0"
+  terraformProvider: "aws@~> 2.0",
+  workflowBootstrapSteps: [{ run: `npm install cdktf-project && npx projen${PROJEN_VERSION}` }, { run: 'yarn install --frozen-lockfile' }]
 });
 
 project.synth();
