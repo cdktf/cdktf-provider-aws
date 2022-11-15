39aa77c0ed8cbc66f7d9d9eb9216692f35a5ed0b
team-tf-cdk <84392119+team-tf-cdk@users.noreply.github.com>
2022-11-16 00:13:54 +0100

    chore(deps)!: Updated  CDKTF version to `0.14.0` (#828)
    Triggered by https://github.com/cdktf/cdktf-repository-manager/actions/runs/3474087374

--- .projenrc.js
+++ .projenrc.js
@@ -2,7 +2,7 @@ const { CdktfProviderProject } = require("@cdktf/provider-project");
 const project = new CdktfProviderProject({
   useCustomGithubRunner: true,
   terraformProvider: "aws@~> 4.0",
-  cdktfVersion: "^0.13.0",
+  cdktfVersion: "^0.14.0",
   constructsVersion: "^10.0.0",
   minNodeVersion: "14.17.0",
   jsiiVersion: "^1.53.0",
