bb2fa15df973b5b9d51e427254ed6afeeeeb98e1
team-tf-cdk <84392119+team-tf-cdk@users.noreply.github.com>
2023-06-03 03:40:50 +0200

    chore(deps): upgrade dependencies (#1055)
    Upgrades project dependencies. See details in [workflow run].
    
    [Workflow Run]: https://github.com/cdktf/cdktf-provider-aws/actions/runs/5160805008
    
    ------
    
    *Automatically created by projen via the "upgrade-main" workflow*

--- src/data-aws-cognito-user-pools/index.ts
+++ src/data-aws-cognito-user-pools/index.ts
@@ -1,3 +1,8 @@
+/**
+ * Copyright (c) HashiCorp, Inc.
+ * SPDX-License-Identifier: MPL-2.0
+ */
+
 // https://registry.terraform.io/providers/hashicorp/aws/4.66.1/docs/data-sources/cognito_user_pools
 // generated from terraform resource schema
 
--- src/data-aws-ec2-client-vpn-endpoint/index.ts
+++ src/data-aws-ec2-client-vpn-endpoint/index.ts
@@ -1,3 +1,8 @@
+/**
+ * Copyright (c) HashiCorp, Inc.
+ * SPDX-License-Identifier: MPL-2.0
+ */
+
 // https://registry.terraform.io/providers/hashicorp/aws/4.66.1/docs/data-sources/ec2_client_vpn_endpoint
 // generated from terraform resource schema
 
