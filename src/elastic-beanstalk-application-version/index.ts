6127a2ee14a78ca00a7b7a565a61a3e8d34df73b
team-tf-cdk <github-team-tf-cdk@hashicorp.com>
2024-07-04 03:25:36 +0000

    chore: self mutation
    Signed-off-by: team-tf-cdk <github-team-tf-cdk@hashicorp.com>

--- src/elastic-beanstalk-application-version/index.ts
+++ src/elastic-beanstalk-application-version/index.ts
@@ -1,3 +1,8 @@
+/**
+ * Copyright (c) HashiCorp, Inc.
+ * SPDX-License-Identifier: MPL-2.0
+ */
+
 // https://registry.terraform.io/providers/hashicorp/aws/5.57.0/docs/resources/elastic_beanstalk_application_version
 // generated from terraform resource schema
 
