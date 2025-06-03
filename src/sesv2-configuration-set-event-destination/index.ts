2d37ef69e2141430a6817a5214e152b248588f20
team-tf-cdk <github-team-tf-cdk@hashicorp.com>
2025-06-03 14:53:18 +0000

    chore: self mutation
    Signed-off-by: team-tf-cdk <github-team-tf-cdk@hashicorp.com>

--- src/sesv2-configuration-set-event-destination/index.ts
+++ src/sesv2-configuration-set-event-destination/index.ts
@@ -1,3 +1,8 @@
+/**
+ * Copyright (c) HashiCorp, Inc.
+ * SPDX-License-Identifier: MPL-2.0
+ */
+
 // https://registry.terraform.io/providers/hashicorp/aws/5.99.1/docs/resources/sesv2_configuration_set_event_destination
 // generated from terraform resource schema
 
