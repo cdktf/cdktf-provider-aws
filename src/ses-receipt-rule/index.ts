0aa3e5c36bafe8a7a0921f548be44158549ac353
github-team-tf-cdk <github-team-tf-cdk@hashicorp.com>
2025-06-03 15:31:35 +0000

    chore(deps): update  by cdktf/cdktf-repository-manager@909bd84

--- src/ses-receipt-rule/index.ts
+++ src/ses-receipt-rule/index.ts
@@ -1,8 +1,3 @@
-/**
- * Copyright (c) HashiCorp, Inc.
- * SPDX-License-Identifier: MPL-2.0
- */
-
 // https://registry.terraform.io/providers/hashicorp/aws/5.99.1/docs/resources/ses_receipt_rule
 // generated from terraform resource schema
 
