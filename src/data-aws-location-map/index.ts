2309e0b8ce4534a87414df069143744448b86a21
github-team-tf-cdk <github-team-tf-cdk@hashicorp.com>
2025-06-03 12:01:12 +0000

    chore(deps): update  by cdktf/cdktf-repository-manager@4c005a5

--- src/data-aws-location-map/index.ts
+++ src/data-aws-location-map/index.ts
@@ -1,8 +1,3 @@
-/**
- * Copyright (c) HashiCorp, Inc.
- * SPDX-License-Identifier: MPL-2.0
- */
-
 // https://registry.terraform.io/providers/hashicorp/aws/5.99.1/docs/data-sources/location_map
 // generated from terraform resource schema
 
